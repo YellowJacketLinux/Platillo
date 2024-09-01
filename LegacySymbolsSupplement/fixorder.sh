3bdf063890031468ecc151b4de11145a0fe74d39
YellowJacketLinux <anymouseprophet@gmail.com>
2024-09-01 00:31:14 -0700

    bug fix

--- LegacySymbolsSupplement/fixorder.sh
+++ LegacySymbolsSupplement/fixorder.sh
@@ -25,7 +25,7 @@ function getAltFileName {
 
 pushd SVG
 for c in C D E; do
-  for a in 0 1 2 3 4 5 6 7 8 9 A B C F; do
+  for a in 0 1 2 3 4 5 6 7 8 9 A B C D E F; do
     for b in 0 1 2 3 4 5 6 7 8 9 A B C D E F; do
       UNICODE="1C${c}${a}${b}"
       filename="`getFileName ${UNICODE}`"
